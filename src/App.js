3fe2fc4d9760fcdaf5843b74a747d70e8c74fba6
PeibolRam <ramirezvazquezp@gmail.com>
2023-03-18 21:52:32 -0600

    favicon

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          Proximamente
+          Próximamente
         </h1>
       </header>
     </div>
